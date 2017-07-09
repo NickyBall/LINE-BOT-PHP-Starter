d68fee166c80a41fe6536e4c06bf4849192ebc71
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2017-07-09 17:51:46 +0700

    fix use path

--- storage.php
+++ storage.php
@@ -2,8 +2,8 @@
 
 require __DIR__."/vendor/autoload.php";
 
-use \MicrosoftAzure\Storage\Common\ServicesBuilder;
-use \MicrosoftAzure\Storage\Common\ServiceException;
+use MicrosoftAzure\Storage\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Common\ServiceException;
 
 
 $connectionString = 'DefaultEndpointsProtocol=https;AccountName=roundplatform;AccountKey=4TIBx0yc648KFnuMsLjXMoT/IqFfCW3/X5ZhzifcKBEydn5qxYY2741M9krgp7wDu2JW1/AD1PyCMspywUYKuw==;EndpointSuffix=core.windows.net';
