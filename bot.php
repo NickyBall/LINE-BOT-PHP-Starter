31470bfed2f205892d3192d1e64e0e108329dff3
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2017-07-09 16:51:07 +0700

    echo bot first attemp

--- bot.php
+++ bot.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello I am bot.";
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$response = $bot->replyText('<reply token>', 'hello!');
 ?>
